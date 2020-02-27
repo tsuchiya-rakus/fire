4dd9022e30885f5e5faf6fc79138e03ddf98f721
tsuchiya <hiromu.tsuchiya@rakus-partners.co.jp>
2020-02-27 13:50:02 +0900

    firstCommit

---
+++ src/firefirefire/Lesson1.java
@@ -0,0 +1,17 @@
+package firefirefire;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				if (i * j < 10) {
+					System.out.print(" " + i * j + " ");
+				} else {
+					System.out.print(i * j + " ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
